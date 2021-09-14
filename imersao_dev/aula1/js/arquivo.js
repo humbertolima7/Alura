7e0cd3dd1f610201b11ba35bf4ef75f40a885a5b
Humberto Lima <87452157+humbertolima7@users.noreply.github.com>
2021-09-13 22:47:35 -0300

    Add files via upload

---
+++ imersao_dev/aula1/js/arquivo.js
@@ -0,0 +1,19 @@
+var nome = "Humberto"
+var notaDoPrimeiroBimestre = 9.4355
+var notaDoSegundoBimestre = 7.234
+var notaDoTerceiroBimestre = 4.234
+var notaDoQuartoBimestre = 2.183
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+var notaFixada = notaFinal.toFixed(1) //comando para fixar 1 casa decimal apos o ponto
+
+console.log("Bem vindo a imersão dev " + nome)
+console.log(notaFixada)
+
+//Revisão: variaveis, strings, console.log, toFixed, operações matemáticas, concatenação +
+
+// Desafios desta aula!
+// Dependendo da nota, mostrar se a aluna ou aluno foi aprovada(o) ou não
+// Alterar o fundo da tela da maneira que achar mais legal 
+// Imprimir na própria página o resultado, ao invés do console
+// Criar um conversor de temperaturas entre farenheit e celcius
+// Colocar a conta inteira da média em apenas uma linha
